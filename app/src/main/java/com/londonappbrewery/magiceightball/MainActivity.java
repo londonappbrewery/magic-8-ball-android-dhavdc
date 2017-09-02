38fc3af8056becbbd10069938c97ab76950f00d7
dhavdc <dhavrutsky@gmail.com>
2017-09-02 12:56:31 -0400

    -Added new launcher icons -Added layout for eight ball -Added code for eight ball

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_ask;
+        final ImageView img_ball;
+        btn_ask = (Button) findViewById(R.id.btn_ask);
+        img_ball = (ImageView) findViewById(R.id.img_ball);
+        final int[] ballimagearray = {
+            R.drawable.ball1,
+            R.drawable.ball2,
+            R.drawable.ball3,
+            R.drawable.ball4,
+            R.drawable.ball5,
+        };
+        btn_ask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int randomnumber;
+                Random randomgen = new Random();
+                randomnumber = randomgen.nextInt(5);
+                img_ball.setImageResource(ballimagearray[randomnumber]);
+
+            }
+        });
     }
 }
